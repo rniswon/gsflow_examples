d60a268d8673a31dd53a8228d23cfbef6e5e1f24
Steve Regan <rsregan@usgs.gov>
2016-11-15 16:51:17 -0700

    removed _32 bat files use PCG instead of NWT in gsflow.control

--- sagehen/windows/gsflow_prms_climate_hru_32.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-..\..\..\bin\gsflow_32 .\prms_climate_hru.control
-ECHO.
-ECHO Run complete. Please press enter when you want to continue.
-PAUSE>NUL
--- sagehen/windows/gsflow_prms_map_results_32.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-..\..\..\bin\gsflow_32 .\gsflow_prms.control -set mapOutON_OFF 1
-ECHO.
-ECHO Run complete. Please press enter when you want to continue.
-PAUSE>NUL
