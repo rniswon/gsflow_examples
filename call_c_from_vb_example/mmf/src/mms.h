0dcba15e8ae1079383dad0404c34a737d5118885
emorway-usgs <emorway@usgs.gov>
2017-03-08 16:57:08 -0800

    Successfully called C (not C++) from VB.NET

--- call_c_from_vb_example/mmf/src/mms.h
+++
@@ -1,9 +0,0 @@
-/*
- *  $Id: mms.h 5090 2009-03-24 18:11:08Z rsregan $
- */
-
-#include "defs.h"
-#include "structs.h"
-#include "globals.h"
-#include "protos.h"
-
--- call_c_from_vb_example/mmf_lite/call_c_from_vb/called_c/called_c.cpp
+++ call_c_from_vb_example/mmf_lite/call_c_from_vb/called_c/called_c.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 // using namespace System;
 
-extern "C" __declspec(dllexport)
+extern "C" __declspec(dllexport)  // This is the main difference between "Compile as C code" and "Default"
             int add(int a, int b);
 
 int add(int a, int b)
