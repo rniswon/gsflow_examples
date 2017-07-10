dc1f68e492508b6f538ea5a17f14717977bca948
Steve Regan <rsregan@usgs.gov>
2017-07-10 14:02:40 -0600

    put in relative path for exe

--- sagehen_3lay_modsim/windows/gsflow.bat
+++ sagehen_3lay_modsim/windows/gsflow.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\bin\gsflow .\gsflow.control
+..\..\..\modsim.git\MODSIM_GSFLOW_C\bin\x86\Release\MODSIM_GSFLOW_C .\gsflow.control
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
