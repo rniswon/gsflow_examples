30d417a6ba51349fe00522782ac27fbf5df9179a
Steve Regan <rsregan@usgs.gov>
2017-07-11 16:56:57 -0600

    fixed path to exe, had gsflow_release.exe instead of gsflow.ext

--- sagehen/windows/gsflow.bat
+++ sagehen/windows/gsflow.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\..\bin\gsflow_release .\gsflow.control
+..\..\..\bin\gsflow .\gsflow.control
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
--- sagehen/windows/gsflow_prms.bat
+++ sagehen/windows/gsflow_prms.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\..\bin\gsflow_release .\gsflow_prms.control
+..\..\..\bin\gsflow .\gsflow_prms.control
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
