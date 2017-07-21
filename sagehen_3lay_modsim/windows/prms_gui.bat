960ab20e52b6ee0818f7220b3a0cce65a6270997
Steve Regan <rsregan@usgs.gov>
2017-07-21 11:13:10 -0600

    removed stats_output_file so warning doesn't come to screen updated PRMS-only bat files

--- sagehen_3lay_modsim/windows/prms_gui.bat
+++ sagehen_3lay_modsim/windows/prms_gui.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
-..\..\bin\gsflow_prms5 -C.\sagehen_2lay.control -print
-java -cp ..\..\dist\oui4.jar oui.mms.gui.Mms .\sagehen.control
+
+..\..\..\bin\prms_5  .\gsflow_prms.control -print
+
+java -cp C:\Workspace\dist\oui4.jar oui.mms.gui.Mms .\gsflow_prms.control
+
 ECHO.
 ECHO Run complete. Please press enter to continue.
 PAUSE>NUL
